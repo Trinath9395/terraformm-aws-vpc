6c6f6d3712926012f4d1d41358b22e03c094034e
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-08 07:52:27 +0530

    fresh clean repo

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  resourse_name = "${var.project_name}-${var.environment}"
+
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "vpc_cidr" {
+  
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "common_tags" {
+  type = map 
+  #default = {}
+}
+
+variable "vpc_tags" {
+  default = {}
+}
